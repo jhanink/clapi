45c6a651b0d7fa4a343c6cb31a2cbe9528d2aa9c
Joe Hanink <jhanink@walmartlabs.com>
2015-05-14 15:03:05 -0700

    add support for get-customer by email

--- api-call-configurations/stage/get-customer.js
+++ api-call-configurations/stage/get-customer.js
@@ -1,11 +1,15 @@
 module.exports = function (state) {
   var service = require("../include/service")(state);
-  if (!state.args.customerId) {
+  var customerId = state.args.customerId;
+  if (!customerId) {
     console.log("---> missing customerId");return;
   }
 
+  var isEmail = customerId.indexOf("@") > 0;
+
   var options = {
-    url: 'http://ca.stg0.ca-services-cdc.prod.walmart.com/ca-app/services/customers/' + state.args.customerId,
+    url: 'http://ca.stg0.ca-services-cdc.prod.walmart.com/ca-app/services/customers/'
+      + (isEmail?'emails/':'') + customerId,
     method: 'GET'
   };
 
