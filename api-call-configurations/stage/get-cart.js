d44043627f47a448c3b7302c3e2a82a7d9e45291
Joe Hanink <jhanink@walmartlabs.com>
2015-05-17 00:19:15 -0700

    check for cartId arg

--- api-call-configurations/stage/get-cart.js
+++ api-call-configurations/stage/get-cart.js
@@ -2,6 +2,10 @@ module.exports = function (state) {
   var service = require("../include/service")(state);
   var cartId = state.args.cartId;
 
+  if (!cartId) {
+    console.log("---> missing cartId");return;
+  }
+
   var options = {
     url: 'http://cartservice-app.stg1.pangeasvcscart.services.glb.prod.walmart.com/cart-service-app/cart/' + cartId,
     method: 'GET'
