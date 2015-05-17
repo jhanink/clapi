c68e63c900b04e0457abfbcf6af01a465d969990
Joe Hanink <jhanink@walmartlabs.com>
2015-05-17 00:19:49 -0700

    check for customerId arg

--- api-call-configurations/stage/create-cart.js
+++ api-call-configurations/stage/create-cart.js
@@ -2,6 +2,10 @@ module.exports = function (state) {
   var service = require("../include/service")(state);
   var customerId = state.args.customerId;
 
+  if (!customerId) {
+    console.log("---> missing customerId");return;
+  }
+
   var options = {
     url: 'http://cartservice-app.stg1.pangeasvcscart.services.glb.prod.walmart.com/cart-service-app/cart',
     method: 'POST'
