b4cd4479fbf4ec9234c4e52727773055bff64cc5
Joe Hanink <jhanink@walmartlabs.com>
2015-06-09 17:31:07 -0700

    * add app bar menu click handler * add left nav bar toggle

--- webpack.config.js
+++ webpack.config.js
@@ -23,22 +23,29 @@ var node_modules = path.resolve(__dirname, 'node_modules');
 var pathToJquery = path.resolve(node_modules, 'jquery/dist/jquery.min.js');*/
 
 var config = {
-  entry: path.resolve(__dirname, 'app/main.js'),
+  entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
   /*resolve: {
     alias: {
       'react': pathToReact
     }
   },*/
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    alias: {},
+    extensions: ['', '.jsx', '.js']
+  },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
     /*noParse: [pathToReact, pathToJquery],*/
-    loaders: [{
-      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-    }]
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel'},
+      { test: /\.css$/, loader: 'style-loader!css-loader' }, // use ! to chain loaders
+      { test: /\.png$/, loader: "url-loader?limit=100000&mimetype=image/png" }
+      /*{ test: /\.jsx$/, loader: 'jsx-loader?harmony' }*/
+      ]
   }
 };
 
