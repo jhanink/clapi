dc71a6f218b5c8488f47d49732314fac31b88ad2
Joe Hanink <jhanink@walmartlabs.com>
2015-06-11 01:32:56 -0700

    remove cruft from top

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,3 @@
-/*
-var path = require('path');
-var config = {
-  entry: path.resolve(__dirname, 'app/main.js'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-    }]
-  }
-};
-
-module.exports = config;*/
-
 
 var path = require('path');
 var node_modules = path.resolve(__dirname, 'node_modules');
