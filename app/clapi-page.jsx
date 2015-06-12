3c268ac5f205ee5a132b912451c275ebbf3c72c5
Joe Hanink <jhanink@walmartlabs.com>
2015-06-11 18:37:27 -0700

    slight code-cleanup

--- app/clapi-page.jsx
+++ app/clapi-page.jsx
@@ -1,26 +1,30 @@
 let React = require("react");
-let mui = require("material-ui");
 let Router = require("react-router");
 let RouteHandler = Router.RouteHandler;
 
+let mui = require("material-ui");
 let AppBar = mui.AppBar;
 let ThemeManager = new mui.Styles.ThemeManager();
 ThemeManager.setTheme(ThemeManager.types.LIGHT);
-
-let ClapiNavMenu = require("./clapi-nav-menu.jsx");
+var SvgIcon = mui.SvgIcon;
 
 let injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
+
+let ClapiNavMenu = require("./clapi-nav-menu.jsx");
 
 module.exports = React.createClass({
   render () {
     return (
       <div>
         <ClapiNavMenu ref="leftNavMenu"/>
-        <AppBar title="CLAPI - using react, material design, and webpack" onLeftIconButtonTouchTap={this._onTapMenu}/>
+        <AppBar title={this._getTitle()} onLeftIconButtonTouchTap={this._onTapMenu} />
         <RouteHandler/>
       </div>
     );
+  },
+  _getTitle() {
+    return "CLAPI - using react, material design, and webpack";
   },
   _onTapMenu() {
     this.refs.leftNavMenu._toggle();
