5e160c34820049eb4611f7d84a21469eed3a6b95
Joe Hanink <jhanink@walmartlabs.com>
2015-07-04 11:43:03 -0700

    suppress result json inspector prior to gift card request

--- app/components/CreateGiftCardView.jsx
+++ app/components/CreateGiftCardView.jsx
@@ -15,7 +15,7 @@ var LinearProgressTag = undefined;
 module.exports = React.createClass({
   getInitialState() {
     return {
-      data: {},
+      data: undefined,
       isFetching: false
     }
   },
@@ -45,7 +45,11 @@ module.exports = React.createClass({
           primary={true}
           onClick={this._handleClick}
           style={{marginBottom: '30px'}}/>
-        <Inspector data={this.state.data}/>
+        {
+          this.state.data
+              ? <Inspector data={this.state.data}/>
+              : <div></div>
+        }
       </div>
     );
   },
