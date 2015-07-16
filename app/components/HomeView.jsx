ea63e76f5d42521c60fc89befde98181e3d208a1
Joe Hanink <jhanink@walmartlabs.com>
2015-07-16 03:53:36 -0700

    * add demo gif to clapi ui homepage

--- app/components/HomeView.jsx
+++ app/components/HomeView.jsx
@@ -1,13 +1,20 @@
 let React     = require("react");
 
-let styles = {color: '#CCC', textAlign: 'center'};
+let styles = {color: '#999', textAlign: 'center', font: '50px Optima '};
 
 class HomeView extends React.Component {
   render() {
     return (
       <div>
         <h2 style={styles}>
-          Those crazy squirrels are nuts!
+          Welcome to CLAPI UI
+          <p style={{font: '30px Optima'}}>
+          click the hamburger to get started
+          </p>
+
+          <div style={{display:'inline-block',border:'3px solid orange',padding:'20px',margin:'50px 150px',borderRadius:'25px'}}>
+            <img src="/images/clapi-ui.gif" width="60%" height="50%"/>
+          </div>
         </h2>
       </div>
     );
