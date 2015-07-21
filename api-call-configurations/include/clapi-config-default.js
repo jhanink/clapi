48ad234aebab2f11be8b1697bd787a1328942bc2
Joe Hanink <jhanink@walmartlabs.com>
2015-07-21 00:49:16 -0700

    enable default for config cid

--- api-call-configurations/include/clapi-config-default.js
+++ api-call-configurations/include/clapi-config-default.js
@@ -1,6 +1,6 @@
 module.exports = {
   "customer": {
-    //"cid": "5d456dc1-c81e-4314-86b3-7b32dd31f64d",
+    "cid": "5d456dc1-c81e-4314-86b3-7b32dd31f64d",
     "email": "node-1@wm.com"
   },
   "purchaseContractEndpoint": "http://xoservice-app.stg1.pangaeasvcsxo.services.prod.walmart.com/checkoutservice/v1/purchasecontracts/",
