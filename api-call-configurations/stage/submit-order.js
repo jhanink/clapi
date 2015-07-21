03b116c3165b4fa415cafdfe4dbe695263a4a323
Joe Hanink <jhanink@walmartlabs.com>
2015-07-21 00:34:15 -0700

    * use config for cid

--- api-call-configurations/stage/submit-order.js
+++ api-call-configurations/stage/submit-order.js
@@ -1,8 +1,14 @@
+var clapiConfig = require("../include/clapi-config");
+
 module.exports = function (state) {
   var service = require("../include/service")(state);
+  if (!state.args.pcId) {
+    console.log("---> missing pcId");return;
+  }
 
   var options = {
-    url: 'http://xoservice-app.stg1.pangaeasvcsxo.services.prod.walmart.com/checkoutservice/v1/purchasecontracts/f7db98f5-6d6c-4a01-bcae-dcebec179ecc/submit',
+    url: 'http://xoservice-app.stg1.pangaeasvcsxo.services.prod.walmart.com/checkoutservice/v1/purchasecontracts/'
+      + state.args.pcId + '/submit',
     method: 'POST'
   };
 
@@ -14,7 +20,7 @@ module.exports = function (state) {
   service.setLocaleId("eng_USA");
 
   service.setServiceEnv("stg1");
-  service.setConsumerUserId("5d456dc1-c81e-4314-86b3-7b32dd31f64d");
+  service.setConsumerUserId(clapiConfig.customer.cid);
   service.setConsumerId("b2d8d5fd-db75-4b2f-8b98-3a417b4c1d10");
   service.setAuthSignature("EGHKfpuQzEz4cABRyQ/8byPVHVIzI9VqXDX4tvuEGEnmJkT1flIYTCnx1m0RDCihridZ0zf+9xxU9r3xnbLgKHe2yTiFRFRIHa1XiUOaQ8ry2JoOF4sQrD9PcwjiqqHGvL2aaD/jUV4xLQ+fajIM2+AW57BQRuDyZA9Y+llMB2E=");
   service.setKeyVersion(1);
