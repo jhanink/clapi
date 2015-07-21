b91b511d6b05b0a16413740dc487543223a93ab1
Joe Hanink <jhanink@walmartlabs.com>
2015-07-21 00:43:15 -0700

    cleaner code org

--- api-call-configurations/stage/config-set-pc-endpoint.js
+++ api-call-configurations/stage/config-set-pc-endpoint.js
@@ -2,6 +2,9 @@ var fs = require("fs");
 var path = require("path");
 var config = require("../include/clapi-config");
 
+var userHome = process.env['HOME'];
+var clapiUserConfigFilePath = path.join(userHome, "/clapi-config-override.json");
+
 module.exports = function (state) {
   var url = state.args.url;
   if (!url) {
@@ -10,9 +13,5 @@ module.exports = function (state) {
   }
 
   config.purchaseContractEndpoint = url;
-
-  var userHome = process.env['HOME'];
-  var clapiUserConfigFilePath = path.join(userHome, "/clapi-config-override.json");
-  fs.writeFileSync(clapiUserConfigFilePath, JSON.stringify(data));
+  fs.writeFileSync(clapiUserConfigFilePath, JSON.stringify(config));
 };
-
