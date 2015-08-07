644aff1e761c8d351996cf9c983c4495cc7db50a
Joe Hanink <jhanink@walmartlabs.com>
2015-08-06 22:23:52 -0700

    * add filterOptions={{ignoreCase:true}} as prop to json Inspector

--- app/components/AddToCartView.jsx
+++ app/components/AddToCartView.jsx
@@ -53,7 +53,7 @@ module.exports = React.createClass({
               style={{marginBottom: '30px'}}/>
           {
             this.state.data
-                ? <Inspector data={this.state.data}/>
+                ? <Inspector data={this.state.data} filterOptions={{ignoreCase:true}}/>
                 : <div></div>
           }
         </div>
